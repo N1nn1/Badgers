c44e630d590ac9b02eeb97fd22dfe05490cdabd0
N1nn1 <64173061+N1nn1@users.noreply.github.com>
2022-08-28 16:52:58 +0200

    Sour berries generation reimplementation

--- src/main/java/com/ninni/badgers/Badgers.java
+++ src/main/java/com/ninni/badgers/Badgers.java
@@ -5,11 +5,16 @@ import com.ninni.badgers.block.BadgersBlocks;
 import com.ninni.badgers.entity.BadgersEntities;
 import com.ninni.badgers.item.BadgersItems;
 import com.ninni.badgers.sound.BadgersSoundEvents;
+import com.ninni.badgers.wold.gen.BadgersWorldGenFeatures;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.biome.BiomeKeys;
+import net.minecraft.world.gen.GenerationStep;
 
 public class Badgers implements ModInitializer {
 	public static final String MOD_ID = "badgers";
@@ -24,5 +29,7 @@ public class Badgers implements ModInitializer {
 			BadgersSoundEvents.class,
 			BadgersBlocks.class
 		);
+
+		BadgersWorldGenFeatures.PATCH_SOUR_BERRIES_PLACED.getKey().ifPresent(placedFeatureRegistryKey -> BiomeModifications.addFeature(BiomeSelectors.includeByKey(BiomeKeys.MEADOW), GenerationStep.Feature.VEGETAL_DECORATION, placedFeatureRegistryKey));
 	}
 }
